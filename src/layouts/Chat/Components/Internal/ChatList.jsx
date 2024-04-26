3a7ee098b00f5fa53cb0436a116e9ce0b52aa5f2
Luis Daniel <luisdaniel.riosb@gmail.com>
2024-04-26 12:51:36 -0600

    fixed chat sidelyaout display of chats

--- src/layouts/Chat/Components/Internal/ChatList.jsx
+++ src/layouts/Chat/Components/Internal/ChatList.jsx
@@ -2,58 +2,108 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function ChatList({ chat, auth }) {
-  console.log(chat, auth);
   return (
-    <>
-      {chat.data?.map((user, i) => (
-        <div>
-          {user.user_id !== auth.id ? (
-            <Link to={`/chat/${chat.chat_id}`}>
-              <div className="flex hover:bg-[#f0f0f0] border-b border-grisDisabled hover:rounded-xl px-5 py-3 my-3">
-                <div className="w-1/6">
-                  <img
-                    src="https://i.scdn.co/image/ab6761610000e5eb153fc7a135b27d664160204b"
-                    width="48px"
-                    className="rounded-full"
-                  />
-                </div>
-                <div className="w-4/6 text-left">
-                  <div>
+    <div>
+      {chat.map((user, i) => {
+        console.log(user.mensaje);
+        return (
+          <>
+            {Number(user.user_id) === Number(auth.id) ? (
+              ""
+            ) : (
+              <Link key={i} to={`/chat/${chat.chat_id}`}>
+                <div className="flex hover:bg-[#f0f0f0] border-b border-grisDisabled hover:rounded-xl px-5 py-3 my-3">
+                  <div className="w-1/6">
+                    <img
+                      src="https://i.scdn.co/image/ab6761610000e5eb153fc7a135b27d664160204b"
+                      width="48px"
+                      className="rounded-full"
+                    />
+                  </div>
+                  <div className="w-4/6 text-left">
                     <span className="font-roboto text-sm font-medium text-grisText">
                       {user.title}
                     </span>
-                  </div>
-                  <div className="mt-[-3px] ">
-                    <span className="font-roboto font-normal text-xs text-grisText">
-                      {" "}
-                      {user.mensaje}{" "}
-                    </span>
-                  </div>
-                </div>
-                <div className="w-1/6">
-                  <div className="text-center">
-                    <span className="text-[10px] font-roboto font-normal text-[#BDBDBD] ">
-                      {" "}
-                      {user.latest}{" "}
-                    </span>
-                  </div>
-                  <div className="text-center">
-                    {user.count !== 0 && (
-                      <span className="font-roboto font-medium text-sm bg-[#00A259] text-white py-1 px-2 rounded-full">
-                        {user.count}
+                    <div className="mt-[-3px] ">
+                      <span className="font-roboto font-normal text-xs text-grisText">
+                        {user.mensaje.mensaje}
                       </span>
-                    )}
+                    </div>
+                  </div>
+                  <div className="w-1/6">
+                    <div className="text-center">
+                      <span className="text-[10px] font-roboto font-normal text-[#BDBDBD] ">
+                        {user?.latest}
+                      </span>
+                    </div>
+                    <div className="text-center">
+                      {user?.count !== 0 && (
+                        <span className="font-roboto font-medium text-sm bg-[#00A259] text-white py-1 px-2 rounded-full">
+                          {user?.count}
+                        </span>
+                      )}
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          ) : (
-            <>No</>
-          )}
-        </div>
-      ))}
-    </>
+              </Link>
+            )}
+          </>
+        );
+      })}
+    </div>
   );
 }
 
 export default ChatList;
+
+{
+  /* <>
+  {chat.data?.map((user, i) => (
+    <div>
+      {user.user_id !== auth.id ? (
+        <Link to={`/chat/${chat.chat_id}`}>
+          <div className="flex hover:bg-[#f0f0f0] border-b border-grisDisabled hover:rounded-xl px-5 py-3 my-3">
+            <div className="w-1/6">
+              <img
+                src="https://i.scdn.co/image/ab6761610000e5eb153fc7a135b27d664160204b"
+                width="48px"
+                className="rounded-full"
+              />
+            </div>
+            <div className="w-4/6 text-left">
+              <div>
+                <span className="font-roboto text-sm font-medium text-grisText">
+                  {user.title}
+                </span>
+              </div>
+              <div className="mt-[-3px] ">
+                <span className="font-roboto font-normal text-xs text-grisText">
+                  {" "}
+                  {user.mensaje}{" "}
+                </span>
+              </div>
+            </div>
+            <div className="w-1/6">
+              <div className="text-center">
+                <span className="text-[10px] font-roboto font-normal text-[#BDBDBD] ">
+                  {" "}
+                  {user.latest}{" "}
+                </span>
+              </div>
+              <div className="text-center">
+                {user.count !== 0 && (
+                  <span className="font-roboto font-medium text-sm bg-[#00A259] text-white py-1 px-2 rounded-full">
+                    {user.count}
+                  </span>
+                )}
+              </div>
+            </div>
+          </div>
+        </Link>
+      ) : (
+        <>No</>
+      )}
+    </div>
+  ))}
+</>; */
+}
--- src/layouts/Chat/LayoutChat.jsx
+++ src/layouts/Chat/LayoutChat.jsx
@@ -7,7 +7,6 @@ import ChatList from "./Components/Internal/ChatList";
 import { pusherClient } from "@/lib/pusher";
 
 function LayoutChat() {
-
   const { users, chats, user } = useLoaderData();
 
   const [initialData, setInitialData] = useState(chats.data);
@@ -21,40 +20,51 @@ function LayoutChat() {
     });
 
     return () => {
-        pusherClient.unsubscribe("get-chat-list");
+      pusherClient.unsubscribe("get-chat-list");
     };
   }, []);
 
-  return (
+  console.log(chatListPusher);
 
+  return (
     <div className="flex h-full px-4 font-roboto pb-4">
       <div className="flex flex-col rounded-xl bg-gris gap-4 w-[450px] shrink-0">
         {/* top block */}
         <Tabs defaultValue="internal" className="w-fullpt-6">
-            <TabsList className="bg-transparent w-full mb-3">
-                <TabsTrigger className="rounded-none text-sm font-normal data-[state=active]:text-primarioBotones bg-transparent data-[state=active]:font-medium data-[state=active]:border-b-2 data-[state=active]:bg-transparent data-[state=active]:border-blue-500 font-roboto  text-grisSubText border-b-2 border-slate-300 p-3" value="internal">INTERNAL</TabsTrigger>
-                <TabsTrigger className="rounded-none text-sm font-normal data-[state=active]:text-primarioBotones data-[state=active]:font-medium data-[state=active]:border-b-2 data-[state=active]:bg-transparent data-[state=active]:border-blue-500 font-roboto  text-grisSubText border-b-2 border-slate-300 p-3" value="whatsapp">WHATSAPP</TabsTrigger>
-                <TabsTrigger className="rounded-none text-sm font-normal data-[state=active]:text-primarioBotones data-[state=active]:font-medium data-[state=active]:border-b-2 data-[state=active]:bg-transparent data-[state=active]:border-blue-500 font-roboto  text-grisSubText border-b-2 border-slate-300 p-3" value="meta">META</TabsTrigger>
-            </TabsList>
-            <TabsContent value="internal" className="h-full">
-                <InternalSearch 
-                  users={users.data}
-                />
-                <div className="px-5 bg-[#FBFBFB]">
-                  {chatListPusher?.map((chat, i) => (
-                      <ChatList
-                        chat={chat}
-                        auth={user.data}
-                      />
-                  ))}
-                </div>
-            </TabsContent>
-            <TabsContent value="whatsapp">
-                <span>Es</span>
-            </TabsContent>
-            <TabsContent value="meta">
-                <span>ghj</span>
-            </TabsContent>
+          <TabsList className="bg-transparent w-full mb-3">
+            <TabsTrigger
+              className="rounded-none text-sm font-normal data-[state=active]:text-primarioBotones bg-transparent data-[state=active]:font-medium data-[state=active]:border-b-2 data-[state=active]:bg-transparent data-[state=active]:border-blue-500 font-roboto  text-grisSubText border-b-2 border-slate-300 p-3"
+              value="internal"
+            >
+              INTERNAL
+            </TabsTrigger>
+            <TabsTrigger
+              className="rounded-none text-sm font-normal data-[state=active]:text-primarioBotones data-[state=active]:font-medium data-[state=active]:border-b-2 data-[state=active]:bg-transparent data-[state=active]:border-blue-500 font-roboto  text-grisSubText border-b-2 border-slate-300 p-3"
+              value="whatsapp"
+            >
+              WHATSAPP
+            </TabsTrigger>
+            <TabsTrigger
+              className="rounded-none text-sm font-normal data-[state=active]:text-primarioBotones data-[state=active]:font-medium data-[state=active]:border-b-2 data-[state=active]:bg-transparent data-[state=active]:border-blue-500 font-roboto  text-grisSubText border-b-2 border-slate-300 p-3"
+              value="meta"
+            >
+              META
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="internal" className="h-full">
+            <InternalSearch users={users.data} />
+            <div className="px-5 bg-[#FBFBFB]">
+              {chatListPusher?.map((chat, i) => (
+                <ChatList chat={chat.data} auth={user.data} />
+              ))}
+            </div>
+          </TabsContent>
+          <TabsContent value="whatsapp">
+            <span>Es</span>
+          </TabsContent>
+          <TabsContent value="meta">
+            <span>ghj</span>
+          </TabsContent>
         </Tabs>
       </div>
       <Outlet />
