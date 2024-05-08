be9022440664f725df447e8e829fc972afe12374
Luis Daniel <luisdaniel.riosb@gmail.com>
2024-05-08 11:35:25 -0600

    chnaged font size and weight selects

--- src/layouts/Masters/FormComponents/select.jsx
+++ src/layouts/Masters/FormComponents/select.jsx
@@ -3,7 +3,14 @@ import * as React from "react";
 import Select from "react-select";
 
 function SelectRouter({ options, name, placeholder }) {
-  return <Select options={options} name={name} placeholder={placeholder} />;
+  return (
+    <Select
+      options={options}
+      name={name}
+      placeholder={placeholder}
+      className="text-sm font-light"
+    />
+  );
 }
 
 export default SelectRouter;
