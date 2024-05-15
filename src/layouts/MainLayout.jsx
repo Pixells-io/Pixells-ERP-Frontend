4140e648b8494035ae91fd2c2e3b2170d801d81d
Dekepdevelop <dekepdevelop@gmail.com>
2024-05-15 13:48:28 -0600

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -86,7 +86,7 @@ function MainLayout() {
   const token = Cookies.get("token");
   const { chat, userAuth, notificationsData } = useLoaderData();
 
-  console.log(notifications);
+  console.log(notificationsData);
 
   useEffect(() => {
     async function fetchData() {
