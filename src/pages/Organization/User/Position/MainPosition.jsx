d0f70422a2af4e66cb6bbe909124d34f875af44c
Luis Daniel <luisdaniel.riosb@gmail.com>
2024-06-11 09:57:50 -0600

    mini changes

--- src/pages/Organization/User/Position/MainPosition.jsx
+++ src/pages/Organization/User/Position/MainPosition.jsx
@@ -377,6 +377,7 @@ function MainPosition() {
     const newInputs = skillsInputs.filter((item, i) => index !== i);
     setSkillsInputs(newInputs);
   }
+
   return (
     <div className="flex w-full">
       <div className="ml-4 flex w-full flex-col gap-4 space-y-4 overflow-x-auto rounded-lg bg-gris p-8">
