2282d8c0bea4748eea39dd5df354267159eddaa2
Dekepdevelop <dekepdevelop@gmail.com>
2024-06-20 17:34:36 -0600

    Update MainOrgDev.jsx

--- src/pages/OrgDev/MainOrgDev.jsx
+++ src/pages/OrgDev/MainOrgDev.jsx
@@ -75,22 +75,22 @@ function MainOrgDev() {
 
   return (
     <div className="flex w-full">
-      <div className="flex flex-col bg-gris px-8 py-4 ml-4 rounded-lg gap-4 w-full">
+      <div className="ml-4 flex w-full flex-col gap-4 rounded-lg bg-gris px-8 py-4">
         {/* navigation inside */}
-        <div className="flex gap-4 items-center">
-          <div className="flex gap-2  text-gris2">
-            <div className="w-12 h-12">
+        <div className="flex items-center gap-4">
+          <div className="flex gap-2 text-gris2">
+            <div className="h-12 w-12">
               <IonIcon
                 icon={chevronBack}
                 size="large"
-                className="bg-blancoBox p-1 rounded-3xl"
+                className="rounded-3xl bg-blancoBox p-1"
               ></IonIcon>
             </div>
-            <div className="w-12 h-12">
+            <div className="h-12 w-12">
               <IonIcon
                 icon={chevronForward}
                 size="large"
-                className="bg-blancoBox p-1 rounded-3xl"
+                className="rounded-3xl bg-blancoBox p-1"
               ></IonIcon>
             </div>
           </div>
@@ -99,11 +99,11 @@ function MainOrgDev() {
         {/* top content */}
         <div className="flex items-center gap-4">
           <div>
-            <h2 className="font-poppins font-bold text-xl text-[#44444F]">
+            <h2 className="font-poppins text-xl font-bold text-[#44444F]">
               ORGANIZATION DEVELOPMENT
             </h2>
           </div>
-          <div className="flex gap-3 text-[#8F8F8F] items-center font-roboto">
+          <div className="flex items-center gap-3 font-roboto text-[#8F8F8F]">
             {/* <div className="text-xs">
               {leads?.data.length == 0 ? "0" : leads?.data.length}{" "}
               {leads?.data.length == 1 ? "lead" : "leads"}
@@ -118,13 +118,13 @@ function MainOrgDev() {
           </div>
         </div>
         <div>
-          <p className="font-poppins font-bold text-xl text-[#44444F]">
+          <p className="font-poppins text-xl font-bold text-[#44444F]">
             Inductions
           </p>
           <IonIcon
             icon={addCircleOutline}
             size="large"
-            className="text-primarioBotones mt-5"
+            className="mt-5 text-primarioBotones"
             onClick={() => setModalCreateInduccion(true)}
           ></IonIcon>
         </div>
@@ -134,82 +134,82 @@ function MainOrgDev() {
           positions={positions.data}
           areas={areas.data}
         />
-        <div className="bg-blancoBg rounded-lg pt-2">
+        <div className="rounded-lg bg-blancoBg pt-2">
           <div className="flex flex-col justify-center">
-            <div className="grid grid-cols-8 w-full py-2 px-4 text-center">
-              <div className="col-span-2 text-left pl-4 flex items-center">
-                <p className="text-grisText font-semibold text-sm">NOMBRE</p>
+            <div className="grid w-full grid-cols-8 px-4 py-2 text-center">
+              <div className="col-span-2 flex items-center pl-4 text-left">
+                <p className="text-sm font-semibold text-grisText">NOMBRE</p>
               </div>
               <div className="flex items-center">
-                <p className="text-grisText font-semibold text-sm">TIPO</p>
+                <p className="text-sm font-semibold text-grisText">TIPO</p>
               </div>
               <div className="flex items-center">
-                <p className="text-grisText font-semibold text-sm">AREAS</p>
+                <p className="text-sm font-semibold text-grisText">AREAS</p>
               </div>
-              <div className="flex items-center ">
-                <p className="text-grisText font-semibold text-sm">
+              <div className="flex items-center">
+                <p className="text-sm font-semibold text-grisText">
                   RESPONSABLE
                 </p>
               </div>
               <div className="flex items-center justify-center">
-                <p className="text-grisText font-semibold text-sm">ARCHIVOS</p>
+                <p className="text-sm font-semibold text-grisText">ARCHIVOS</p>
               </div>
               <div className="flex items-center justify-center">
-                <p className="text-grisText font-semibold text-sm">EXAMEN</p>
+                <p className="text-sm font-semibold text-grisText">EXAMEN</p>
               </div>
               <div className="flex items-center justify-center">
-                <p className="text-grisText font-semibold text-sm">HISTORIAL</p>
+                <p className="text-sm font-semibold text-grisText">HISTORIAL</p>
               </div>
             </div>
-            <div className="flex flex-col py-2 px-4 text-center gap-2">
+            <div className="flex flex-col gap-2 px-4 py-2 text-center">
               {inductionsPusher?.map((row, i) => (
-                <div key={i} className="grid grid-cols-8 w-full border-t py-4">
-                  <div className="col-span-2 text-left pl-4 flex items-center">
-                    <p className="text-grisHeading text-xs">{row.name}</p>
+                <div key={i} className="grid w-full grid-cols-8 border-t py-4">
+                  <div className="col-span-2 flex items-center pl-4 text-left">
+                    <p className="text-xs text-grisHeading">{row.name}</p>
                   </div>
                   <div className="flex items-center">
-                    <p className="text-grisHeading text-xs">{row.type}</p>
+                    <p className="text-xs text-grisHeading">{row.type}</p>
                   </div>
                   <div className="flex items-center pl-4">
-                    <p className="text-grisHeading text-xs">{row.area}</p>
+                    <p className="text-xs text-grisHeading">{row.area}</p>
                   </div>
                   <div className="flex items-center">
-                    <p className="text-grisHeading text-xs">
+                    <p className="text-xs text-grisHeading">
                       {row.responsable}
                     </p>
                   </div>
-                  <div className="flex justify-center items-center">
+                  <div className="flex items-center justify-center">
                     <p
                       className={
                         row.archive
-                          ? "bg-[#00A25940] text-[#00A259] text-xs rounded-full py-1 px-3 w-fit"
-                          : "bg-[#7794F940] text-[#7794F9] text-xs rounded-full py-1 px-3 w-fit"
+                          ? "w-fit rounded-full bg-[#00A25940] px-3 py-1 text-xs text-[#00A259]"
+                          : "w-fit rounded-full bg-[#7794F940] px-3 py-1 text-xs text-[#7794F9]"
                       }
                     >
                       Archivos
                     </p>
                   </div>
                   {row.examen === false ? (
-                    <div className="flex justify-center items-center">
+                    <div className="flex items-center justify-center">
                       <NavLink
                         to={`/org-development/induction/create/${row?.id}`}
                       >
-                        <p className="bg-[#7794F940] text-[#7794F9] text-xs rounded-full py-1 px-3 w-fit">
+                        <p className="w-fit rounded-full bg-[#7794F940] px-3 py-1 text-xs text-[#7794F9]">
                           Exámen
                         </p>
                       </NavLink>
                     </div>
                   ) : (
-                    <div className="flex justify-center items-center">
+                    <div className="flex items-center justify-center">
                       <NavLink to={`/org-development/exam/${row?.examen_id}`}>
-                        <p className="bg-[#00A25940] text-[#00A259] text-xs rounded-full py-1 px-3 w-fit">
+                        <p className="w-fit rounded-full bg-[#00A25940] px-3 py-1 text-xs text-[#00A259]">
                           Exámen
                         </p>
                       </NavLink>
                     </div>
                   )}
 
-                  <div className="flex justify-center items-center">
+                  <div className="flex items-center justify-center">
                     <NavLink to={`/org-development/induction/${row?.id}`}>
                       <IonIcon
                         icon={informationCircle}
@@ -223,9 +223,9 @@ function MainOrgDev() {
           </div>
         </div>
       </div>
-      <div className="w-[280px] flex flex-col gap-6 bg-gris px-8 py-4 ml-4 rounded-lg shrink-0">
+      <div className="ml-4 flex w-[280px] shrink-0 flex-col gap-6 rounded-lg bg-gris px-8 py-4">
         <div className="flex justify-center">
-          <p className="text-grisHeading text-lg font-poppins font-semibold">
+          <p className="font-poppins text-lg font-semibold text-grisHeading">
             Accesos Rápidos
           </p>
         </div>
@@ -234,27 +234,27 @@ function MainOrgDev() {
           {PEOPLE.map((item, i) => (
             <div key={i} className="flex">
               <div className="flex w-1/3 flex-col items-center gap-1">
-                <div className="flex w-12 h-12 items-center justify-center ">
-                  <Avatar className="rounded-lg h-full w-full">
+                <div className="flex h-12 w-12 items-center justify-center">
+                  <Avatar className="h-full w-full rounded-lg">
                     <AvatarImage src="https://github.com/shadcn.png" />
                     <AvatarFallback>CN</AvatarFallback>
                   </Avatar>
                 </div>
                 {item.status == "Pending" ? (
-                  <span className="text-[11px] bg-[#FAA36440] text-[#FAA364] px-2 py-[2px] w-fit rounded-full">
+                  <span className="w-fit rounded-full bg-[#FAA36440] px-2 py-[2px] text-[11px] text-[#FAA364]">
                     {item.status}
                   </span>
                 ) : (
-                  <span className="text-[11px] bg-[#7794F940] text-[#7794F9] px-2 py-[2px] w-fit rounded-full">
+                  <span className="w-fit rounded-full bg-[#7794F940] px-2 py-[2px] text-[11px] text-[#7794F9]">
                     {item.status}
                   </span>
                 )}
               </div>
               <div>
-                <p className="text-grisText font-medium text-base">
+                <p className="text-base font-medium text-grisText">
                   {item.name}
                 </p>
-                <span className="font-medium text-[10px] text-grisSubText line-clamp-none ">
+                <span className="line-clamp-none text-[10px] font-medium text-grisSubText">
                   {item.position}
                 </span>
               </div>
