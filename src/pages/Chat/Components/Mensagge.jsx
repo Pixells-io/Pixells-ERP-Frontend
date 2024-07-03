299eadc43c0a8203d439bf69666c9512e8251a8e
Dekepdevelop <dekepdevelop@gmail.com>
2024-07-03 09:55:57 -0600

    Update Mensagge.jsx

--- src/pages/Chat/Components/Mensagge.jsx
+++ src/pages/Chat/Components/Mensagge.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { IonIcon } from "@ionic/react";
 import { checkmarkDoneOutline } from "ionicons/icons";
-import VisibilitySensor from 'react-visibility-sensor';
+import VisibilitySensor from "react-visibility-sensor";
 import Cookies from "js-cookie";
 import { json } from "react-router-dom";
 
@@ -11,61 +11,65 @@ function MenssageCard({ data, user }) {
   return (
     <>
       {msg.user == user ? (
-        <div className="w-full flex justify-end">
-            <div className="bg-[#E4F0FF] mb-1 rounded-t-xl rounded-s-xl w-fit px-2 py-1  text-clip overflow-hidden ...">
-                <div>
-                <span className="font-roboto font-normal text-sm text-[#44444F]">
-                    {msg.mensaje}
-                </span>
-                </div>
-                <div className="mt-[-8px] flex justify-end">
-                <div>
-                    <span className="text-[10px] font-normal font-roboto text-[#8F8F8F]">
-                    {msg.date}
-                    </span>
-                </div>
-                <div className="mt-[3px] ml-1">
-                    {/* THE MESSAGE IS READ */}
-                    {msg.read === "1" && (
-                    <IonIcon
-                        icon={checkmarkDoneOutline}
-                        size="small"
-                        className="text-[#5B89FF]"
-                    ></IonIcon>
-                    )}
-                    {/* THE MESSAGE IS NOT READ */}
-                    {msg.read !== "1" && (
-                    <IonIcon
-                        icon={checkmarkDoneOutline}
-                        size="small"
-                        className="text-[#8F8F8F]"
-                    ></IonIcon>
-                    )}
-                </div>
-                </div>
+        <div className="flex w-full justify-end">
+          <div className="mb-1 w-fit overflow-hidden text-ellipsis rounded-s-xl rounded-t-xl bg-[#E4F0FF] px-2 py-1">
+            <div className="overflow-hidden text-ellipsis">
+              <span
+                className="font-roboto text-sm font-normal text-[#44444F]"
+                title={msg.mesaje}
+              >
+                {msg.mensaje}
+              </span>
             </div>
+            <div className="mt-[-8px] flex justify-end">
+              <div>
+                <span className="font-roboto text-[10px] font-normal text-[#8F8F8F]">
+                  {msg.date}
+                </span>
+              </div>
+              <div className="ml-1 mt-[3px]">
+                {/* THE MESSAGE IS READ */}
+                {msg.read === "1" && (
+                  <IonIcon
+                    icon={checkmarkDoneOutline}
+                    size="small"
+                    className="text-[#5B89FF]"
+                  ></IonIcon>
+                )}
+                {/* THE MESSAGE IS NOT READ */}
+                {msg.read !== "1" && (
+                  <IonIcon
+                    icon={checkmarkDoneOutline}
+                    size="small"
+                    className="text-[#8F8F8F]"
+                  ></IonIcon>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       ) : (
         <VisibilitySensor
-            onChange={(isVisible) => {
-                ShowAction(msg);
-          }}>
-            <div className="w-full  flex justify-start">
-                <div className="bg-[#F0F0F0] mb-1 rounded-t-xl rounded-r-xl w-fit px-2 py-1  text-clip overflow-hidden ...">
+          onChange={(isVisible) => {
+            ShowAction(msg);
+          }}
+        >
+          <div className="flex w-full justify-start">
+            <div className="... mb-1 w-fit overflow-hidden text-clip rounded-r-xl rounded-t-xl bg-[#F0F0F0] px-2 py-1">
+              <div>
+                <span className="font-roboto text-sm font-normal text-[#44444F]">
+                  {msg.mensaje}
+                </span>
+              </div>
+              <div className="mt-[-8px] flex justify-end">
                 <div>
-                    <span className="font-roboto font-normal text-sm text-[#44444F] ">
-                    {msg.mensaje}
-                    </span>
+                  <span className="font-roboto text-[10px] font-normal text-[#8F8F8F]">
+                    {msg.date}
+                  </span>
                 </div>
-                <div className="mt-[-8px] flex justify-end">
-                    <div>
-                    <span className="text-[10px] font-normal font-roboto text-[#8F8F8F]">
-                        {msg.date}
-                    </span>
-                    </div>
-                </div>
-                </div>
+              </div>
             </div>
+          </div>
         </VisibilitySensor>
       )}
     </>
@@ -75,22 +79,21 @@ function MenssageCard({ data, user }) {
 export default MenssageCard;
 
 export async function ShowAction(mensaje) {
+  const info = {
+    msj_id: mensaje.id,
+  };
 
-    const info = {
-        msj_id: mensaje.id,
-    };
-
-    if (mensaje.read == "0") {
-        const response = await fetch(
-            `${import.meta.env.VITE_SERVER_URL}chat/read-message`,
-            {
-              method: "POST",
-              body: JSON.stringify(info),
-              headers: {
-                Authorization: "Bearer " + Cookies.get("token"),
-              },
-            }
-          );
-    }
-    return 1;
+  if (mensaje.read == "0") {
+    const response = await fetch(
+      `${import.meta.env.VITE_SERVER_URL}chat/read-message`,
+      {
+        method: "POST",
+        body: JSON.stringify(info),
+        headers: {
+          Authorization: "Bearer " + Cookies.get("token"),
+        },
+      },
+    );
   }
+  return 1;
+}
