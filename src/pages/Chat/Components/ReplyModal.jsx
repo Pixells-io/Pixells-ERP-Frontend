1149d12d3ce93599bfe59de07375eedb586adaf9
Dekepdevelop <dekepdevelop@gmail.com>
2024-07-04 20:21:15 -0600

    Chat Msg reply and resend
    In this commit will create the repply and resend views

---
+++ src/pages/Chat/Components/ReplyModal.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+
+import { Form, useNavigation } from "react-router-dom";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+function ReplyModal({ modal, setModal }) {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (navigation.state === "idle") {
+      setModal(false);
+    }
+  }, [navigation.state]);
+
+  return (
+    <Dialog open={modal} onOpenChange={setModal}>
+      <DialogContent className="overflow-auto p-0 sm:max-w-[425px]">
+        <DialogHeader className="border-b pt-2">
+          <DialogTitle className="px-8 py-4 font-poppins font-semibold text-grisHeading">
+            Reply Message
+          </DialogTitle>
+        </DialogHeader>
+        <Form
+          id="area-form"
+          className="flex h-full w-full flex-col gap-3 px-6"
+          action="/organization"
+          method="post"
+        >
+          <div className="flex w-full flex-col gap-3 rounded-lg p-4 font-roboto">
+            <div className="flex w-full flex-col gap-3 pb-4 font-light"></div>
+          </div>
+        </Form>
+        <DialogFooter className="px-10 pb-6">
+          <Button
+            form="area-form"
+            className="justify-normal rounded-lg bg-primarioBotones px-6 py-2 font-roboto text-xs font-semibold"
+          >
+            Reply
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default ReplyModal;
