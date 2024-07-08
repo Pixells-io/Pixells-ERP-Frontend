72a621390964aafbf7153cf128989f72f8af4ae0
Dekepdevelop <dekepdevelop@gmail.com>
2024-07-08 11:34:06 -0600

    pdate

---
+++ src/pages/Organization/User/FormImport.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
+import SelectRouter from "../../../layouts/Masters/FormComponents/select";
+
+import { Form, useNavigation } from "react-router-dom";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { IonIcon } from "@ionic/react";
+import { add, closeCircle } from "ionicons/icons";
+import InputRouter from "@/layouts/Masters/FormComponents/input";
+import DropzoneFile from "@/components/dropzone-files";
+
+const DAYS = [
+  { label: "Areas", value: "1" },
+  { label: "Process Areas", value: "2" },
+  { label: "Type of Turn", value: "3" },
+  { label: "Positions", value: "4" },
+  { label: "Positions Coordinate", value: "5" },
+  { label: "Position Authority", value: "6" },
+  { label: "Position Responsability", value: "7" },
+  { label: "Position Perfil", value: "8" },
+  { label: "Positions Conocimientos", value: "9" },
+  { label: "Languages", value: "10" },
+];
+
+function FormImport({ modal, setModal }) {
+  const [processValue, setProcessValue] = useState([]);
+  const navigation = useNavigation();
+  const [processInputs, setProcessInputs] = useState([
+    {
+      proceso: "",
+    },
+  ]);
+
+  useEffect(() => {
+    if (navigation.state === "idle") {
+      setModal(false);
+    }
+  }, [navigation.state]);
+
+  return (
+    <Dialog open={modal} onOpenChange={setModal}>
+      <DialogContent className="overflow-auto p-0 sm:max-w-[425px]">
+        <DialogHeader className="border-b pt-2">
+          <DialogTitle className="px-8 py-4 font-poppins font-semibold text-grisHeading">
+            Import Data
+          </DialogTitle>
+        </DialogHeader>
+        <Form
+          id="area-form"
+          className="flex h-full w-full flex-col gap-3 px-6"
+          action="/organization"
+          method="post"
+          encType="multipart/form-data"
+        >
+          <div className="flex w-full flex-col gap-3 rounded-lg p-4 font-roboto">
+            <div className="flex w-full flex-col gap-3 pb-4 font-light">
+              <input type="hidden" name="type" value={4} />
+              <Select
+                name="type"
+                placeholder={"Working Days"}
+                options={DAYS}
+                isMulti={false}
+              />
+              <input type="file" name="document" />
+            </div>
+          </div>
+          <DialogFooter className="px-10 pb-6">
+            <Button
+              type="submit"
+              className="justify-normal rounded-lg bg-primarioBotones px-6 py-2 font-roboto text-xs font-semibold"
+            >
+              Save
+            </Button>
+          </DialogFooter>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default FormImport;
--- src/pages/Organization/utils.js
+++ src/pages/Organization/utils.js
@@ -401,7 +401,7 @@ export async function editUser(data) {
     `${import.meta.env.VITE_SERVER_URL}organization/edit-user`,
     {
       method: "POST",
-      body: FormData,
+      body: formData,
       headers: {
         Authorization: "Bearer " + Cookies.get("token"),
         "Content-Type": "multipart/form-data",
@@ -466,4 +466,19 @@ export async function savePermission(position, permision, module) {
   );
 
   return response.json();
+}
+
+export async function importOrganization(data) {
+  const response = await fetch(
+    `${import.meta.env.VITE_SERVER_URL}organization/import`,
+    {
+      method: "POST",
+      body: data,
+      headers: {
+        Authorization: "Bearer " + Cookies.get("token"),
+      },
+    },
+  );
+
+  return response;
 }
