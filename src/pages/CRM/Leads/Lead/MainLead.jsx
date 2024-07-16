0211fc7c07f9b5e39a4b83660e4076114a7cf3fd
Dekepdevelop <dekepdevelop@gmail.com>
2024-07-16 10:56:40 -0600

    Update

--- src/pages/CRM/Leads/Lead/MainLead.jsx
+++ src/pages/CRM/Leads/Lead/MainLead.jsx
@@ -16,8 +16,19 @@ import {
 import CardFollowUp from "./CardFollowUp";
 
 function MainLead() {
-  const [lead, services, info, follow_ups, main_lead] = useOutletContext();
+  const [
+    lead,
+    services,
+    info,
+    follow_ups,
+    main_lead,
+    closing,
+    pay,
+    onBoarding,
+  ] = useOutletContext();
   const step = main_lead.step;
+
+  console.log(onBoarding);
 
   return (
     <div className="flex w-full">
@@ -206,10 +217,148 @@ function MainLead() {
 
         {/* cards */}
         <div className="flex h-full w-full flex-col items-center overflow-auto bg-blancoBg">
-          {/* Here is the cards */}
           {follow_ups?.map((follow, i) => (
             <CardFollowUp info={follow} />
           ))}
+          {/* Here is the cards */}
+          {closing != null ? (
+            <div className="my-6 flex w-[510px] shrink-0 flex-col rounded-lg bg-gris pb-2 shadow-sm drop-shadow-sm">
+              {/* card header */}
+              <div className="flex justify-between border-b-[0.5px] border-[#D7D7D7]">
+                <div className="flex items-center gap-2 p-1">
+                  <IonIcon
+                    icon={document}
+                    size="large"
+                    className="text-primario"
+                  ></IonIcon>
+
+                  <p className="text-[15px] font-medium text-gris2">
+                    Closing Up
+                  </p>
+                </div>
+                <div className="flex items-center gap-2 p-1 text-grisSubText">
+                  <div className="flex items-center gap-1">
+                    <IonIcon icon={time} className=""></IonIcon>
+                    <span className="text-[10px]">{closing.date} </span>
+                  </div>
+                </div>
+              </div>
+              {/* card content */}
+              <div className="flex justify-between px-3">
+                <div className="mt-2 flex gap-6">
+                  <div className="flex flex-col gap-1">
+                    <p className="text-xs font-medium text-grisSubText">
+                      Recurrent Pay
+                    </p>
+                    <p className="text-xs font-medium text-grisSubText">
+                      Total Ammount
+                    </p>
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <span className="text-xs text-grisHeading">
+                      Recurrent Pay
+                    </span>
+                    <span className="text-xs text-grisHeading">
+                      ${closing.month_billing}
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-between px-3">
+                {closing.sales.map((sale, i) => (
+                  <div className="mt-3 flex gap-6" key={i}>
+                    <div className="flex flex-col gap-1">
+                      <p className="text-xs font-medium text-grisSubText">
+                        Service {i + 1}
+                      </p>
+                      <p className="text-xs font-medium text-grisSubText">
+                        Price
+                      </p>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                      <span className="text-xs text-grisHeading">
+                        {sale.name}
+                      </span>
+                      <span className="text-xs text-grisHeading">
+                        ${sale.ammount}
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : null}
+          {pay != null ? (
+            <div className="my-6 flex w-[510px] shrink-0 flex-col rounded-lg bg-gris pb-2 shadow-sm drop-shadow-sm">
+              {/* card header */}
+              <div className="flex justify-between border-b-[0.5px] border-[#D7D7D7]">
+                <div className="flex items-center gap-2 p-1">
+                  <IonIcon
+                    icon={card}
+                    size="large"
+                    className="text-primario"
+                  ></IonIcon>
+
+                  <p className="text-[15px] font-medium text-gris2">Pay</p>
+                </div>
+                <div className="flex items-center gap-2 p-1 text-grisSubText">
+                  <div className="flex items-center gap-1">
+                    <IonIcon icon={time} className=""></IonIcon>
+                    <span className="text-[10px]">{closing.date} </span>
+                  </div>
+                </div>
+              </div>
+              {/* card content */}
+              <div className="flex justify-between px-3">
+                <div className="mt-2 flex gap-6">
+                  <div className="flex flex-col gap-1">
+                    <p className="text-xs font-medium text-grisSubText">Date</p>
+                    <p className="text-xs font-medium text-grisSubText">
+                      Total
+                    </p>
+                    <p className="text-xs font-medium text-grisSubText">
+                      Comments
+                    </p>
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <span className="text-xs text-grisHeading">
+                      $ {pay.total}
+                    </span>
+                    <span className="text-xs text-grisHeading">
+                      {pay.comments}
+                    </span>
+                    <span className="text-xs text-grisHeading">
+                      {pay.date_of_pay}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : null}
+          {onBoarding != null ? (
+            <div className="my-6 flex w-[510px] shrink-0 flex-col rounded-lg bg-gris pb-2 shadow-sm drop-shadow-sm">
+              {/* card header */}
+              <div className="flex justify-between border-b-[0.5px] border-[#D7D7D7]">
+                <div className="flex items-center gap-2 p-1">
+                  <IonIcon
+                    icon={checkmarkCircle}
+                    size="large"
+                    className="text-primario"
+                  ></IonIcon>
+
+                  <p className="text-[15px] font-medium text-gris2">
+                    On Boarding
+                  </p>
+                </div>
+                <div className="flex items-center gap-2 p-1 text-grisSubText">
+                  <div className="flex items-center gap-1">
+                    <IonIcon icon={time} className=""></IonIcon>
+                    <span className="text-[10px]">{onBoarding.date} </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
