a13ecd69c5bbf2b19a8d2de77bfe0f40d3190442
Dekepdevelop <dekepdevelop@gmail.com>
2024-07-23 18:35:52 -0600

    Update

--- src/layouts/Chat/utils.js
+++ src/layouts/Chat/utils.js
@@ -13,8 +13,31 @@ export async function SearchAction(data) {
       headers: {
         Authorization: "Bearer " + Cookies.get("token"),
       },
-    }
+    },
   );
 
   return response;
+}
+
+export async function saveGroup(data) {
+  const info = {
+    chat: data.get("chat"),
+  };
+
+  console.log(data);
+
+  /*const response = await fetch(
+    `${import.meta.env.VITE_SERVER_URL}chat/search`,
+    {
+      method: "POST",
+      body: JSON.stringify(info),
+      headers: {
+        Authorization: "Bearer " + Cookies.get("token"),
+      },
+    },
+  );
+
+  return response;*/
+
+  return 1;
 }
