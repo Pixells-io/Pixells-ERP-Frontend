94d8c6a33f108013ec2e04c7e029fec696a5a87e
Luis Daniel <65049835+LuisDanielRB@users.noreply.github.com>
2024-08-06 13:13:40 -0600

    Update MediaInformations.jsx

--- src/pages/Chat/Components/MediaInformations.jsx
+++ src/pages/Chat/Components/MediaInformations.jsx
@@ -1,7 +1,21 @@
-import { IonIcon } from "@ionic/react";
-import { trash } from "ionicons/icons";
 import React, { useState } from "react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import { IonIcon } from "@ionic/react";
+import { personRemove, personRemoveOutline, trash } from "ionicons/icons";
+
 import ModalConfirmation from "./ModalConfirmation";
+
+import {
+  Calculator,
+  Calendar,
+  CreditCard,
+  Settings,
+  Smile,
+  User,
+  Crown,
+} from "lucide-react";
 
 function MediaInformations({ data }) {
   const [modalConfirmation, setModalConfirmation] = useState(false);
@@ -39,20 +53,30 @@ function MediaInformations({ data }) {
           key={"participants" + index}
           className="flex flex-col gap-2 rounded-t-xl px-6 py-4"
         >
-          <div className="flex items-center justify-between hover:bg-grisBg">
-            <div className="flex items-center gap-4">
-              <img src={participant.img} className="h-14 w-14 rounded-full" />
+          <div className="flex items-center justify-between hover:rounded-lg hover:bg-grisBg">
+            <div className="flex items-center gap-4 py-2 pl-4">
+              <Avatar>
+                <AvatarImage src={participant.img} className="size-10" />
+                <AvatarFallback>
+                  {participant.name} {participant.last_name}
+                </AvatarFallback>
+              </Avatar>
+              {/* <img src={participant.img} className="h-10 w-10 rounded-full" /> */}
               <div>
                 <span className="text-xs font-semibold text-grisText">
                   {participant.name} {participant.last_name}{" "}
                   {participant.second_last_name}
                 </span>
               </div>
+              {participant.admin !== 0 && (
+                <Crown className="size-4 text-[#FFC34E]" />
+              )}
             </div>
+
             <div>
               <IonIcon
-                icon={trash}
-                className="h-8 w-8 text-grisText hover:cursor-pointer hover:text-red-500"
+                icon={personRemove}
+                className="size-5 pr-4 text-grisText hover:cursor-pointer hover:text-red-500"
                 onClick={() => openModalDelete(participant.id)}
               />
             </div>
