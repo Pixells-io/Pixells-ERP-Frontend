1ba57a04b83cabda3390505343d62c960e586100
Luis Daniel <65049835+LuisDanielRB@users.noreply.github.com>
2024-08-07 16:53:39 -0600

    updtes a user org

--- src/pages/Transformation/Kardex/MainKardex.jsx
+++ src/pages/Transformation/Kardex/MainKardex.jsx
@@ -244,7 +244,7 @@ const data = [
 function MainKardex() {
   return (
     <div className="flex w-full">
-      <div className="ml-4 flex w-full flex-col space-y-4 overflow-auto rounded-lg bg-gris px-8 py-4">
+      <div className="ml-4 flex w-full flex-col space-y-4 rounded-lg bg-gris px-8 py-4">
         {/* navigation inside */}
         <div className="flex items-center gap-4">
           <div className="flex gap-2 text-gris2">
@@ -285,48 +285,48 @@ function MainKardex() {
           Kardex
         </p>
 
-          <div className="flex w-full items-center gap-x-12 rounded-xl bg-blancoBg px-6 py-6">
-            <div>
-              <Select name="article" className="h-10">
-                <SelectTrigger className="w-[180px] rounded-xl border border-[#696974] bg-inherit text-xs font-light text-grisSubText">
-                  <SelectValue placeholder="Selecciona el Artículo" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="light">Light</SelectItem>
-                  <SelectItem value="dark">Dark</SelectItem>
-                  <SelectItem value="system">System</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <div>
-              <Input
-                className="w-[100px] rounded-xl border border-[#696974] bg-inherit text-xs font-light"
-                name={`code`}
-                // value={row.amount}
-                placeholder="Código"
-                type="text"
-              />
-            </div>
-            <div>
-              <Select name="store" className="h-10">
-                <SelectTrigger className="w-[180px] rounded-xl border border-[#696974] bg-inherit text-xs font-light text-grisSubText">
-                  <SelectValue placeholder="Almacén" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="light">Light</SelectItem>
-                  <SelectItem value="dark">Dark</SelectItem>
-                  <SelectItem value="system">System</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <div>
-              <h2 className="text-poppins text-sm font-bold text-[#44444F]">
-                Método: Costo Medio
-              </h2>
-            </div>
+        <div className="flex w-full items-center gap-x-12 rounded-xl bg-blancoBg px-6 py-6">
+          <div>
+            <Select name="article" className="h-10">
+              <SelectTrigger className="w-[180px] rounded-xl border border-[#696974] bg-inherit text-xs font-light text-grisSubText">
+                <SelectValue placeholder="Selecciona el Artículo" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
+          <div>
+            <Input
+              className="w-[100px] rounded-xl border border-[#696974] bg-inherit text-xs font-light"
+              name={`code`}
+              // value={row.amount}
+              placeholder="Código"
+              type="text"
+            />
+          </div>
+          <div>
+            <Select name="store" className="h-10">
+              <SelectTrigger className="w-[180px] rounded-xl border border-[#696974] bg-inherit text-xs font-light text-grisSubText">
+                <SelectValue placeholder="Almacén" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div>
+            <h2 className="text-poppins text-sm font-bold text-[#44444F]">
+              Método: Costo Medio
+            </h2>
+          </div>
+        </div>
 
-        <div className="h-full rounded-xl px-4 py-2">
+        <div className="flex h-full overflow-auto">
           <AverageCostTable data={data} columns={AverageCostColumns} />
         </div>
       </div>
