657809cb0311bc4b03345f24c1e3020ca29b6096
Bartolo <123692950+AntonioEAB@users.noreply.github.com>
2024-08-08 13:42:41 -0600

    Update form in Sales/quotes
    update inputsgroup in quotesform

--- src/pages/Sales/Quotes/components/QuoteList.jsx
+++ src/pages/Sales/Quotes/components/QuoteList.jsx
@@ -1,73 +1,117 @@
 import React, { useState, useCallback } from "react";
-import SelectField from "./ui/select";
-import InputField from "./ui/Input";
+import { Label } from "@/components/ui/label";
+import InputField from "@/layouts/Masters/FormComponents/InputField";
+import SelectField from "@/layouts/Masters/FormComponents/SelectField";
+import { IonIcon } from "@ionic/react";
+import { addCircleOutline } from "ionicons/icons";
 import QuoteTable from "./Tabs/QuoteDataTable";
 
-const labels = ["Cliente", "RFC", "Teléfono", "Creación", "Vencimiento"];
+const options = [
+  {
+    label: "Opción 1",
+    value: "opcion1",
+  },
+  {
+    label: "Opción 2",
+    value: "opcion2",
+  },
+  {
+    label: "Opción 3",
+    value: "opcion3",
+  },
+];
 
 const QuoteList = ({ setSubtotal }) => {
-  const [documentNumber,setDocumentNumber]=useState(1);
-  const [selectValues, setSelectValues] = useState({
-    select1: "",
-    select2: "",
-    select3: "",
-    select4: "",
-    select5: "",
-  });
+  const [documentNumber, setDocumentNumber] = useState("1");
+  const [selectedClient, setSelectedClient] = useState("");
+  const [selectedRFC, setSelectedRFC] = useState("");
+  const [selectedPhone, setSelectedPhone] = useState("");
+  const [selectedCreation, setSelectedCreation] = useState("");
+  const [selectedExpiry, setSelectedExpiry] = useState("");
 
-  const handleSelectChange = useCallback((value, num) => {
-    setSelectValues((prev) => ({
-      ...prev,
-      [`select${num}`]: value,
-    }));
-  }, []);
-
-  const handleTotalChange = useCallback((newTotal) => {
-    setSubtotal(newTotal);
-  }, [setSubtotal]);
+  const handleTotalChange = useCallback(
+    (newTotal) => {
+      setSubtotal(newTotal);
+    },
+    [setSubtotal],
+  );
 
   return (
     <div className="rounded-xl bg-white p-4">
-      <div className="flex items-start space-x-4">
-        <div className="flex-grow grid grid-cols-4 gap-4">
-          {labels.slice(0, 4).map((label, index) => (
-            <SelectField
-              key={index}
-              label={label}
-              id={`select${index + 1}`}
-              value={selectValues[`select${index + 1}`]}
-              onChange={(value) => handleSelectChange(value, index + 1)}
-              showAddIcon={index === 0}
-            />
-          ))}
-          <div className="col-span-1 col-start-4 row-start-3">
-            <SelectField
-              label={labels[4]}
-              value={selectValues.select5}
-              onChange={(value) => handleSelectChange(value, 5)}
-            />
-          </div>
-        </div>
-        <div className="w-20">
+      <div className="flex flex-wrap space-x-4 gap-4">
+        <div className="flex flex-col w-10 ">
+          <Label className="mb-1 font-roboto text-sm text-grisText">No.</Label>
           <InputField
-            label="No"
-            id="documentNumber"
+            name="documentNumber"
             value={documentNumber}
             onChange={(e) => setDocumentNumber(e.target.value)}
-            readOnly
-            placeholder="Número automático"
+            readOnly={true}
+          />
+        </div>
+        <div className="flex-1">
+          <Label className="mb-1 font-roboto text-sm text-grisText">Cliente</Label>
+          <SelectField
+            name="cliente"
+            placeholder="Seleccionar"
+            options={options}
+            onChange={(value) => setSelectedClient(value)}
+            value={selectedClient}
+          />
+        </div>
+        <div className="flex pt-4 items-center justify-center">
+          <IonIcon
+            size="small"
+            icon={addCircleOutline}
+            className="text-primarioBotones"
+          />
+        </div>
+        <div className="flex-1">
+          <Label className="mb-1 font-roboto text-sm text-grisText">RFC:</Label>
+          <SelectField
+            name="rfc"
+            placeholder="Seleccionar"
+            options={options}
+            onChange={(value) => setSelectedRFC(value)}
+            value={selectedRFC}
+          />
+        </div>
+        <div className="flex-1">
+          <Label className="mb-1 font-roboto text-sm text-grisText">Teléfono</Label>
+          <SelectField
+            name="telefono"
+            placeholder="Seleccionar"
+            options={options}
+            onChange={(value) => setSelectedPhone(value)}
+            value={selectedPhone}
+          />
+        </div>
+        <div className="flex-1">
+          <Label className="mb-1 font-roboto text-sm text-grisText">Creación</Label>
+          <SelectField
+            name="creacion"
+            placeholder="Seleccionar"
+            options={options}
+            onChange={(value) => setSelectedCreation(value)}
+            value={selectedCreation}
+          />
+        </div>
+        <div className="flex-1">
+          <Label className="mb-1 font-roboto text-sm text-grisText">Vencimiento</Label>
+          <SelectField
+            name="vencimiento"
+            placeholder="Seleccionar"
+            options={options}
+            onChange={(value) => setSelectedExpiry(value)}
+            value={selectedExpiry}
           />
         </div>
       </div>
+
       <div className="mt-6">
         <QuoteTable onTotalChange={handleTotalChange} />
       </div>
     </div>
   );
 };
-
-
-
-
 
 export default QuoteList;
