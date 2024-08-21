57bedc0dade3ef7a7de3bf4f56d1452d1008d766
Luis Daniel <65049835+LuisDanielRB@users.noreply.github.com>
2024-08-20 18:33:04 -0600

    dashboard wip responsive

--- src/pages/Dashboard/Components/TimeManagement.jsx
+++ src/pages/Dashboard/Components/TimeManagement.jsx
@@ -1,99 +1,154 @@
 import React from "react";
 
 function TimeManagement() {
-    return (
-        <div className="bg-blancoBox w-[456px] rounded-xl p-2 h-full shadow-lg">
-            <div className="flex justify-between pb-3 pt-2 pr-[73px] pl-12 ">
-                <span className="text-sm font-medium font-roboto text-grisText">
-                    Week 13
-                </span>
-                <span className="text-sm font-medium font-roboto text-grisText">
-                    12h 46m
-                </span>
-            </div>
-            <div className="bg-[#F0F0F0] rounded-md p-2">
-                <div className="flex border-b pb-3 border-[#D7D7D7]">
-                    <div className="w-2/6 ml-10">
-                        <span className="font-roboto font-light text-grisText">Mon</span>
-                        <br/>
-                        <span className="font-roboto font-medium text-grisText">Apr 08</span>
-                    </div>
-                    <div className="w-2/6 items-center">
-                        <br/>
-                        <span className="font-roboto font-light text-xs text-grisText p-2 border border-[#D7D7D7] rounded-md">10:00</span>&nbsp;<span className="font-roboto font-light text-xs text-grisText p-2 border border-[#D7D7D7] rounded-md">12:33</span>
-                    </div>
-                    <div  className="w-2/6 ml-6">
-                        <span className="font-roboto font-light text-grisText">Total</span>
-                        <br/>
-                        <span className="font-roboto font-medium text-grisText">2h 33m</span>
-                    </div>
-                </div>
-                <div className="flex justify-between border rounded-md p-2 pb-3 bg- border-[#7794f9] bg-[#7794F926]">
-                    <div className="w-2/6 ml-8">
-                        <span className="font-roboto font-light text-grisText">Tue</span>
-                        <br/>
-                        <span className="font-roboto font-medium text-grisText">Apr 09</span>
-                    </div>
-                    <div className="w-2/6 items-center">
-                        <br/>
-                        <span className="font-roboto font-light text-xs text-grisText p-2 border border-[#D7D7D7] rounded-md">10:00</span>&nbsp;<span className="font-roboto font-light text-xs text-grisText p-2 border border-[#D7D7D7] rounded-md">12:33</span>
-                    </div>
-                    <div  className="w-2/6 ml-10">
-                        <span className="font-roboto font-light text-grisText">Total</span>
-                        <br/>
-                        <span className="font-roboto font-medium text-grisText">2h 33m</span>
-                    </div>
-                </div>
-                <div className="flex justify-between border-b pb-3 border-[#D7D7D7]">
-                    <div className="w-2/6 ml-10">
-                        <span className="font-roboto font-light text-grisText">Wen</span>
-                        <br/>
-                        <span className="font-roboto font-medium text-grisText">Apr 10</span>
-                    </div>
-                    <div className="w-2/6 items-center">
-                        <br/>
-                        <span className="font-roboto font-light text-xs text-grisText p-2 border border-[#D7D7D7] rounded-md">10:00</span>&nbsp;<span className="font-roboto font-light text-xs text-grisText p-2 border border-[#D7D7D7] rounded-md">12:33</span>
-                    </div>
-                    <div className="w-2/6 ml-6">
-                        <span className="font-roboto font-light text-grisText">Total</span>
-                        <br/>
-                        <span className="font-roboto font-medium text-grisText">2h 33m</span>
-                    </div>
-                </div>
-                <div className="flex justify-between border-b pb-3 border-[#D7D7D7]">
-                    <div className="w-2/6 ml-10">
-                        <span className="font-roboto font-light text-grisText">Thu</span>
-                        <br/>
-                        <span className="font-roboto font-medium text-grisText">Apr 11</span>
-                    </div>
-                    <div className="w-2/6 items-center">
-                        <br/>
-                        <span className="font-roboto font-light text-xs text-grisText p-2 border border-[#D7D7D7] rounded-md">10:00</span>&nbsp;<span className="font-roboto font-light text-xs text-grisText p-2 border border-[#D7D7D7] rounded-md">12:33</span>
-                    </div>
-                    <div className="w-2/6 ml-6">
-                        <span className="font-roboto font-light text-grisText">Total</span>
-                        <br/>
-                        <span className="font-roboto font-medium text-grisText">2h 33m</span>
-                    </div>
-                </div>
-                <div className="flex justify-between border-b pb-3 border-[#D7D7D7]">
-                    <div className="w-2/6 ml-10">
-                        <span className="font-roboto font-light text-grisText">Fri</span>
-                        <br/>
-                        <span className="font-roboto font-medium text-grisText">Apr 12</span>
-                    </div>
-                    <div className="w-2/6 items-center">
-                        <br/>
-                        <span className="font-roboto font-light text-xs text-grisText p-2 border border-[#D7D7D7] rounded-md">10:00</span>&nbsp;<span className="font-roboto font-light text-xs text-grisText p-2 border border-[#D7D7D7] rounded-md">12:33</span>
-                    </div>
-                    <div className="w-2/6 ml-6">
-                        <span className="font-roboto font-light text-grisText">Total</span>
-                        <br/>
-                        <span className="font-roboto font-medium text-grisText">2h 33m</span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="h-full w-full rounded-xl bg-blancoBox p-2 shadow-lg md:w-[456px]">
+      <div className="flex flex-col justify-between pb-3 pl-12 pr-[73px] pt-2 md:flex-row">
+        <span className="font-roboto text-sm font-medium text-grisText">
+          Week 13
+        </span>
+        <span className="font-roboto text-sm font-medium text-grisText">
+          12h 46m
+        </span>
+      </div>
+
+      <div className="w-ful flex flex-col rounded-md bg-[#F0F0F0] p-2">
+        <div className="flex w-full border-b border-[#D7D7D7] pb-3 md:flex-row">
+          <div className="ml-0 flex w-1/3 flex-col items-center justify-center md:ml-10 md:w-2/6">
+            <span className="font-roboto font-light text-grisText">Mon</span>
+            <br />
+            <span className="font-roboto font-medium text-grisText">
+              Apr 08
+            </span>
+          </div>
+          <div className="flex w-1/3 flex-col items-center gap-2 md:w-2/6 md:flex-row md:gap-0">
+            <br className="hidden md:flex" />
+            <span className="rounded-md border border-[#D7D7D7] p-2 font-roboto text-xs font-light text-grisText">
+              10:00
+            </span>
+            <br className="hidden md:flex" />
+            <span className="rounded-md border border-[#D7D7D7] p-2 font-roboto text-xs font-light text-grisText">
+              12:33
+            </span>
+          </div>
+          <div className="ml-0 flex w-1/3 flex-col items-center justify-center md:ml-6 md:w-2/6">
+            <span className="font-roboto font-light text-grisText">Total</span>
+            <br />
+            <span className="font-roboto font-medium text-grisText">
+              2h 33m
+            </span>
+          </div>
         </div>
-    );
+
+        <div className="flex justify-between rounded-md border border-[#7794f9] bg-[#7794F926] p-2 pb-3 md:flex-row">
+          <div className="ml-0 w-full md:ml-8 md:w-2/6">
+            <span className="font-roboto font-light text-grisText">Tue</span>
+            <br />
+            <span className="font-roboto font-medium text-grisText">
+              Apr 09
+            </span>
+          </div>
+          <div className="flex w-2/6 flex-col items-center gap-2 md:flex-row md:gap-0">
+            <br className="hidden md:flex" />
+            <span className="rounded-md border border-[#D7D7D7] p-2 font-roboto text-xs font-light text-grisText">
+              10:00
+            </span>
+            <br className="hidden md:flex" />
+            <span className="rounded-md border border-[#D7D7D7] p-2 font-roboto text-xs font-light text-grisText">
+              12:33
+            </span>
+          </div>
+          <div className="ml-10 w-2/6">
+            <span className="font-roboto font-light text-grisText">Total</span>
+            <br />
+            <span className="font-roboto font-medium text-grisText">
+              2h 33m
+            </span>
+          </div>
+        </div>
+
+        <div className="flex w-full border-b border-[#D7D7D7] pb-3 md:flex-row">
+          <div className="ml-0 flex w-1/3 flex-col items-center justify-center md:ml-10 md:w-2/6">
+            <span className="font-roboto font-light text-grisText">Mon</span>
+            <br />
+            <span className="font-roboto font-medium text-grisText">
+              Apr 10
+            </span>
+          </div>
+          <div className="flex w-1/3 flex-col items-center gap-2 md:w-2/6 md:flex-row md:gap-0">
+            <br className="hidden md:flex" />
+            <span className="rounded-md border border-[#D7D7D7] p-2 font-roboto text-xs font-light text-grisText">
+              10:00
+            </span>
+            <br className="hidden md:flex" />
+            <span className="rounded-md border border-[#D7D7D7] p-2 font-roboto text-xs font-light text-grisText">
+              12:33
+            </span>
+          </div>
+          <div className="ml-0 flex w-1/3 flex-col items-center justify-center md:ml-6 md:w-2/6">
+            <span className="font-roboto font-light text-grisText">Total</span>
+            <br />
+            <span className="font-roboto font-medium text-grisText">
+              2h 33m
+            </span>
+          </div>
+        </div>
+
+        <div className="flex w-full border-b border-[#D7D7D7] pb-3 md:flex-row">
+          <div className="ml-0 flex w-1/3 flex-col items-center justify-center md:ml-10 md:w-2/6">
+            <span className="font-roboto font-light text-grisText">Mon</span>
+            <br />
+            <span className="font-roboto font-medium text-grisText">
+              Apr 11
+            </span>
+          </div>
+          <div className="flex w-1/3 flex-col items-center gap-2 md:w-2/6 md:flex-row md:gap-0">
+            <br className="hidden md:flex" />
+            <span className="rounded-md border border-[#D7D7D7] p-2 font-roboto text-xs font-light text-grisText">
+              10:00
+            </span>
+            <br className="hidden md:flex" />
+            <span className="rounded-md border border-[#D7D7D7] p-2 font-roboto text-xs font-light text-grisText">
+              12:33
+            </span>
+          </div>
+          <div className="ml-0 flex w-1/3 flex-col items-center justify-center md:ml-6 md:w-2/6">
+            <span className="font-roboto font-light text-grisText">Total</span>
+            <br />
+            <span className="font-roboto font-medium text-grisText">
+              2h 33m
+            </span>
+          </div>
+        </div>
+
+        <div className="flex w-full border-b border-[#D7D7D7] pb-3 md:flex-row">
+          <div className="ml-0 flex w-1/3 flex-col items-center justify-center md:ml-10 md:w-2/6">
+            <span className="font-roboto font-light text-grisText">Mon</span>
+            <br />
+            <span className="font-roboto font-medium text-grisText">
+              Apr 12
+            </span>
+          </div>
+          <div className="flex w-1/3 flex-col items-center gap-2 md:w-2/6 md:flex-row md:gap-0">
+            <br className="hidden md:flex" />
+            <span className="rounded-md border border-[#D7D7D7] p-2 font-roboto text-xs font-light text-grisText">
+              10:00
+            </span>
+            <br className="hidden md:flex" />
+            <span className="rounded-md border border-[#D7D7D7] p-2 font-roboto text-xs font-light text-grisText">
+              12:33
+            </span>
+          </div>
+          <div className="ml-0 flex w-1/3 flex-col items-center justify-center md:ml-6 md:w-2/6">
+            <span className="font-roboto font-light text-grisText">Total</span>
+            <br />
+            <span className="font-roboto font-medium text-grisText">
+              2h 33m
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default TimeManagement;
