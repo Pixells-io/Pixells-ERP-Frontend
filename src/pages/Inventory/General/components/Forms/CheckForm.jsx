75eee01c376c791ddeba877ce2865dbbdbf7c1a3
Bartolo <123692950+AntonioEAB@users.noreply.github.com>
2024-09-04 16:21:02 -0600

    updated Checkform

--- src/pages/Inventory/General/components/Forms/CheckForm.jsx
+++ src/pages/Inventory/General/components/Forms/CheckForm.jsx
@@ -8,17 +8,16 @@ import {
 } from "@/components/ui/select";
 import { Label } from "@/components/ui/label";
 
-const CheckForm = ({ data, setData }) => {
+const CheckForm = ({ suppliers, data, setData }) => {
   const selectClasses =
     "border-gris2-transparent ml-4 w-[50px] rounded-xl border border-none bg-grisBg font-roboto placeholder:text-grisHeading focus-visible:ring-primarioBotones sm:w-96 lg:w-[500px] focus:ring-2 focus:ring-primarioBotones focus:border-transparent";
 
   const handleSelectChange = (name, value) => {
     setData({
       ...data,
-      [name]: value
+      [name]: value,
     });
   };
-
   return (
     <div className="grid grid-cols-1 gap-4">
       {/* Primera columna */}
@@ -26,12 +25,12 @@ const CheckForm = ({ data, setData }) => {
         <div className="flex items-center">
           <Label
             htmlFor="proveedor"
-            className="text-[14px] mb-1 mr-4 w-50 font-roboto text-gris2"
+            className="w-50 mb-1 mr-4 font-roboto text-[14px] text-gris2"
           >
             Proveedor predeterminado
           </Label>
-          <Select 
-            name="proveedor" 
+          <Select
+            name="proveedor"
             value={data.proveedor}
             onValueChange={(value) => handleSelectChange("proveedor", value)}
           >
@@ -39,9 +38,16 @@ const CheckForm = ({ data, setData }) => {
               <SelectValue placeholder="Seleccionar" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="option1">Option 1</SelectItem>
-              <SelectItem value="option2">Option 2</SelectItem>
-              <SelectItem value="option3">Option 3</SelectItem>
+              {suppliers.data.map((proveedor) => (
+                // Verifica que proveedor.id esté definido antes de usarlo
+                <SelectItem
+                key={proveedor.id}
+                  value={ proveedor.id
+                  }
+                >
+                  {proveedor.name}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
         </div>
