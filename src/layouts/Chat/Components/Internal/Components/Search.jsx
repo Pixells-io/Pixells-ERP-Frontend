baf774a764dcabd216af0cda63a2813dcb336c55
Agustin Hernandez Montero <agdehernandezmo@ittepic.edu.mx>
2024-09-06 16:15:34 -0700

    agustin chat select

--- src/layouts/Chat/Components/Internal/Components/Search.jsx
+++ src/layouts/Chat/Components/Internal/Components/Search.jsx
@@ -1,4 +1,7 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import SelectRouter from "@/layouts/Masters/FormComponents/select";
+import { IonIcon } from "@ionic/react";
+import { searchOutline } from "ionicons/icons";
 import React, { useRef } from "react";
 import { Form, useSubmit } from "react-router-dom";
 
@@ -26,13 +29,49 @@ function Search(search) {
       method="post"
     >
       <input type="hidden" name="type_of_function" value="1" hidden readOnly />
-      <Select
-        options={search.users}
-        placeholder="BUSCAR"
-        name="chat"
-        className="rounded-2xl"
-        onChange={() => onInputEnter()}
-      />
+      <div className="flex items-center rounded-3xl px-3 shadow-[0px_0px_8px_1px_rgba(0,0,0,0.15)]">
+        <IonIcon
+          icon={searchOutline}
+          className="h-6 w-6 text-grisText"
+        ></IonIcon>
+        <Select
+          options={search.users}
+          placeholder="BUSCAR"
+          name="chat"
+          className="w-[10px] flex-1 rounded-2xl"
+          getOptionLabel={(option) => {
+            return (
+              <div className="flex items-center gap-x-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src={option?.image} />
+                </Avatar>
+                <h2>{option.label}</h2>
+              </div>
+            );
+          }}
+          filterOption={(option, value) => {
+            return (
+              option.data.label
+                .toLowerCase()
+                .includes(value.toLowerCase())
+            );
+          }}
+          onChange={() => onInputEnter()}
+          styles={{
+            control: (baseStyles, state) => ({
+              ...baseStyles,
+              border: "0px",
+              boxShadow: "none",
+              background: "none",
+            }),
+          }}
+          components={{
+            DropdownIndicator: () => null,
+            IndicatorSeparator: () => null,
+          }}
+        />
+      </div>
+
       {/* <SelectRouter
         options={selectUser}
         name="chat"
