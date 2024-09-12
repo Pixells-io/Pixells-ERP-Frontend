b94d099e480a2bbd42e2a36a5158ce04999d3472
Luis Daniel <65049835+LuisDanielRB@users.noreply.github.com>
2024-09-11 20:18:41 -0600

    more mobile fixes pm

--- src/pages/PManager/Projects.jsx
+++ src/pages/PManager/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
   const { data } = useLoaderData();
 
   return (
-    <div className="flex h-full overflow-auto bg-blancoBg p-10">
+    <div className="flex h-full w-full overflow-auto rounded-2xl bg-blancoBg p-10">
       <div className="flex h-fit flex-wrap items-center gap-8">
         {data?.map((project, i) => (
           <ProjectCard key={i} project={project} />
