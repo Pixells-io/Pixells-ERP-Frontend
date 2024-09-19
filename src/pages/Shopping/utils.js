5e197810cf61e99979f838cae738085c67646f46
Agustin Hernandez Montero <agdehernandezmo@ittepic.edu.mx>
2024-09-19 11:33:13 -0700

    agustin finish requestOrder shopping

--- src/pages/Shopping/utils.js
+++ src/pages/Shopping/utils.js
@@ -498,6 +498,66 @@ export async function saveNewRequestOrder(data) {
   return response.json();
 }
 
+export async function updateRequestOrder(data) {
+
+  let totalRow = data.getAll("totalRow[]");
+  let arrayArticlesNew = [];
+  for (let i = 0; i < totalRow.length; i++) {
+    if(data.get(`id_product[${i}]`) == ""){
+      arrayArticlesNew.push({
+        master_product: data.get(`master_product[${i}]`),
+        variations: data.get(`variations[${i}]`),
+        sub_total: data.get(`sub_total[${i}]`),
+        discount: data.get(`discount[${i}]`),
+        taxes: data.get(`taxes[${i}]`),
+        quantity: data.get(`quantity[${i}]`),
+        unit: data.get(`unitHidden[${i}]`),
+        delivery_date: data.get(`delivery_date[${i}]`),
+        total: data.get(`total[${i}]`),
+      });
+    }
+  }
+
+  let arrayArticlesDelete = [];
+  let productsDelete = data.getAll("productDelete[]");
+ 
+  for (let i = 0; i < productsDelete.length; i++) {
+    arrayArticlesDelete.push({slot_id: productsDelete[i]});
+  }
+
+  const info = {
+    buy_id: data.get("buy_id"),
+    document_number: data.get("document_number"),
+    inventory_id: data.get("inventory_id"),
+    supplier_id: data.get("supplier_id"),
+    document_created: data.get("document_created"),
+    delivery_date: data.get("delivery_date"),
+    payment_condition: data.get("payment_condition"),
+    comments: data.get("comments"),
+    subtotal: data.get("subtotal"),
+    taxes: data.get("taxes"),
+    total: data.get("total"),
+    payment_type: data.get("payment_type"),
+    limit_credit_date: data.get("limit_credit_date"),
+    products: arrayArticlesNew,
+    destroy_products: arrayArticlesDelete,
+  }
+
+
+  const response = await fetch(
+    `${import.meta.env.VITE_SERVER_URL}shopping/edit-buys`,
+    {
+      method: "POST",
+      body: JSON.stringify(info),
+      headers: {
+        Authorization: "Bearer " + Cookies.get("token"),
+      },
+    },
+  );
+
+  return response.json();
+}
+
 export async function destroyRequestOrder(data) {
 
   const info = {
