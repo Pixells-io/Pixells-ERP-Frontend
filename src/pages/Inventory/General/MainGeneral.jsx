b3f6f4ce0584745c15b25eb405cd789d6bc6957d
Luis Daniel <65049835+LuisDanielRB@users.noreply.github.com>
2024-09-23 14:02:46 -0600

    updates inventory

--- src/pages/Inventory/General/MainGeneral.jsx
+++ src/pages/Inventory/General/MainGeneral.jsx
@@ -38,6 +38,7 @@ import ProductAttributeTabs from "./ProductAttributes/ProductAttributeTabs";
 import { Link } from "react-router-dom";
 import { createPusherClient } from "@/lib/pusher";
 import ModalDeleteProduct from "./Modals/ModalDeleteProduct";
+import NavigationHeader from "@/components/navigation-header";
 
 const MainGeneral = () => {
   const { categories, attributes, products } = useLoaderData();
@@ -169,29 +170,9 @@ const MainGeneral = () => {
       />
       <div className="ml-4 flex w-full flex-col space-y-4 rounded-lg bg-gris px-8 py-4">
         {/* navigation inside */}
-        <div className="flex items-center gap-4">
-          <div className="flex gap-2 text-gris2">
-            <div className="h-12 w-12">
-              <IonIcon
-                icon={chevronBack}
-                size="large"
-                className="rounded-3xl bg-blancoBox p-1"
-              ></IonIcon>
-            </div>
-            <div className="h-12 w-12">
-              <IonIcon
-                icon={chevronForward}
-                size="large"
-                className="rounded-3xl bg-blancoBox p-1"
-              ></IonIcon>
-            </div>
-          </div>
-          <div className="font-roboto text-sm text-grisText">
-            <div>Inventory - General</div>
-          </div>
-        </div>
-        {/* top content */}
+        <NavigationHeader />
 
+        {/* top content */}
         <div className="flex items-center gap-4">
           <h2 className="font-poppins text-xl font-bold text-[#44444F]">
             INVENTARIO
