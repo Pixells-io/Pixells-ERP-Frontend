ac557e2849b223146be41de2503eee5f810033d6
Luis Daniel <65049835+LuisDanielRB@users.noreply.github.com>
2024-09-27 13:13:23 -0600

    changes

--- src/pages/Shopping/Orders/NewOrder/Components/ElementGroup.jsx
+++ src/pages/Shopping/Orders/NewOrder/Components/ElementGroup.jsx
@@ -37,6 +37,7 @@ const InputsGroup = ({
         disabled={!isEditable}
         required={true}
       />
+
       <div className="w-full">
         <p className="mb-1 text-[10px] font-normal text-grisText">Almacén</p>
         <Select
@@ -58,30 +59,6 @@ const InputsGroup = ({
           </SelectContent>
         </Select>
       </div>
-
-      {/* Categoría
-            <div className="flex flex-col">
-            <p className="mb-1 text-[10px] font-normal text-grisText">
-              Categoría
-            </p>
-            <Select
-              name="categoria"
-              value={inputsData?.categoria || ""}
-              onValueChange={(value) => handleSelectChange("categoria", value)}
-            >
-              <SelectTrigger className={selectClasses}>
-                <SelectValue placeholder="Seleccionar" />
-              </SelectTrigger>
-              <SelectContent>
-                {Array.isArray(categories?.data) &&
-                  categories?.data.map((category) => (
-                    <SelectItem key={category.id} value={category.id}>
-                      {category.name}
-                    </SelectItem>
-                  ))}
-              </SelectContent>
-            </Select>
-          </div> */}
 
       <div className="w-full">
         <p className="mb-1 text-[10px] font-normal text-grisText">
