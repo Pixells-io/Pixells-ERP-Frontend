220dd0b7bf84a2da0c30633f43d45403b642284d
Bartolo <123692950+AntonioEAB@users.noreply.github.com>
2024-10-01 09:16:27 -0600

    updated date to serviceform
    changes to style date

--- src/pages/Inventory/GeneralServices/Components/Forms/UserForm.jsx
+++ src/pages/Inventory/GeneralServices/Components/Forms/UserForm.jsx
@@ -168,7 +168,7 @@ const UserTab = ({ users }) => {
         ))}
       </div>
 
-      <div className="mt-[500px] flex w-full flex-1 items-end px-6">
+      <div className="mt-10 flex w-full flex-1 items-end px-6">
         <div className="flex w-full justify-between">
           <label className="text-xs font-light text-[#8F8F8F]">Actualizado 07 septiembre 2024</label>
         </div>
