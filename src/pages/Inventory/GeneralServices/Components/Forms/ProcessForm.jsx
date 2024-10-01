182576e67c63b8c9cf72d6cd6cc70d134c096158
Bartolo <123692950+AntonioEAB@users.noreply.github.com>
2024-10-01 09:32:23 -0600

    updated to forms
    changes to style in buttons

--- src/pages/Inventory/GeneralServices/Components/Forms/ProcessForm.jsx
+++ src/pages/Inventory/GeneralServices/Components/Forms/ProcessForm.jsx
@@ -16,7 +16,14 @@ const ProcessTab = () => {
   const [selectEditProcess, setSelectEditProcess] = useState(null);
 
   const addProcess = () => {
-    const newProcess = { title: "", name: "", descripcion: "", active: "0", start: "", end: "" };
+    const newProcess = {
+      title: "",
+      name: "",
+      descripcion: "",
+      active: "0",
+      start: "",
+      end: "",
+    };
     setProcesses([...processes, newProcess]);
     setSelectEditProcess(processes.length);
   };
@@ -50,41 +57,52 @@ const ProcessTab = () => {
   }, [navigation.state]);
 
   return (
-    <div className="flex h-full w-full md:max-h-[620px] flex-col overflow-auto py-4">
+    <div className="flex h-full w-full flex-col overflow-auto py-4 md:max-h-[620px]">
       <div className="overflow-auto px-6">
-        <h2 className="font-poppins text-sm font-medium text-[#44444F]">PROCESOS</h2>
+        <h2 className="font-poppins text-sm font-medium text-[#44444F]">
+          PROCESOS
+        </h2>
 
         <div className="mt-2 flex w-fit items-center gap-x-2">
-          <Button onClick={addProcess} className="flex h-[24px] min-w-[73px] gap-x-0.5 rounded-xl border border-primarioBotones bg-primarioBotones text-[#FFFFFF] px-1.5 text-[11px] font-medium hover:bg-primarioBotones">
-            <IonIcon className="h-5 w-5 text-[#FFFFFF]" icon={add}/>
+          <Button
+            onClick={addProcess}
+            className="flex h-[24px] min-w-[73px] gap-x-0.5 rounded-xl border border-primarioBotones bg-primarioBotones px-1.5 text-[11px] font-medium text-[#FFFFFF] hover:bg-primarioBotones"
+          >
+            <IonIcon className="h-5 w-5 text-[#FFFFFF]" icon={add} />
             Agregar
           </Button>
         </div>
 
         {processes.map((process, index) => (
           <Form className="mt-4" key={index} method="post">
-            <p className="py-2 text-[10px] font-normal text-[#8F8F8F]">PROCESO {index + 1}</p>
+            <p className="py-2 text-[10px] font-normal text-[#8F8F8F]">
+              PROCESO {index + 1}
+            </p>
             <div className="mt-1 grid w-full grid-cols-12 gap-x-8 gap-y-2 border-t border-[#D7D7D7] py-4">
               <div className="col-span-3">
-                <InputForm 
-                  name="title" 
-                  type="text" 
-                  placeholder="Titulo" 
-                  value={process.title} 
-                  onChange={(e) => handleInputChange(e.target.value, "title", index)} 
+                <InputForm
+                  name="title"
+                  type="text"
+                  placeholder="Titulo"
+                  value={process.title}
+                  onChange={(e) =>
+                    handleInputChange(e.target.value, "title", index)
+                  }
                 />
               </div>
               <div className="col-span-3">
                 <SelectRouter placeholder={"Categoría/Acción"} />
               </div>
-              
+
               <div className="col-span-3">
-                <InputForm 
-                  name="descripcion" 
-                  type="text" 
-                  placeholder="Descripcion" 
-                  value={process.descripcion} 
-                  onChange={(e) => handleInputChange(e.target.value, "descripcion", index)} 
+                <InputForm
+                  name="descripcion"
+                  type="text"
+                  placeholder="Descripcion"
+                  value={process.descripcion}
+                  onChange={(e) =>
+                    handleInputChange(e.target.value, "descripcion", index)
+                  }
                 />
               </div>
 
@@ -95,7 +113,9 @@ const ProcessTab = () => {
                     disabled={navigation.state === "submitting"}
                   >
                     <IonIcon className="h-5 w-5" icon={checkmark}></IonIcon>
-                    {navigation.state === "submitting" ? "Submitting..." : "Guardar"}
+                    {navigation.state === "submitting"
+                      ? "Submitting..."
+                      : "Guardar"}
                   </Button>
                 )}
               </div>
@@ -107,20 +127,30 @@ const ProcessTab = () => {
                       className="data-[state=checked]:bg-primarioBotones data-[state=unchecked]:bg-grisDisabled"
                       name="active"
                       checked={process.active === "1"}
-                      onCheckedChange={(e) => handleInputChange(e ? "1" : "0", "active", index)}
+                      onCheckedChange={(e) =>
+                        handleInputChange(e ? "1" : "0", "active", index)
+                      }
                     />
-                    <label className="font-roboto text-xs font-normal text-grisText">Activo</label>
+                    <label className="font-roboto text-xs font-normal text-grisText">
+                      Activo
+                    </label>
                     {!!process.start && !!process.end ? (
                       <div className="flex items-center gap-x-2">
                         <div className="rounded-[8px] bg-gris px-2 py-1">
-                          <label className="text-xs font-light text-[#44444F]">{format(process.start, "PP")}</label>
+                          <label className="text-xs font-light text-[#44444F]">
+                            {format(process.start, "PP")}
+                          </label>
                         </div>
                         <div className="rounded-[8px] bg-gris px-2 py-1">
-                          <label className="text-xs font-light text-[#44444F]">{format(process.end, "PP")}</label>
+                          <label className="text-xs font-light text-[#44444F]">
+                            {format(process.end, "PP")}
+                          </label>
                         </div>
                       </div>
                     ) : (
-                      <label className="font-roboto text-xs font-light text-grisSubText">(Sin periodo de tiempo)</label>
+                      <label className="font-roboto text-xs font-light text-grisSubText">
+                        (Sin periodo de tiempo)
+                      </label>
                     )}
                   </div>
                   <div>
@@ -133,15 +163,25 @@ const ProcessTab = () => {
                         Restablecer
                       </Button>
                     ) : (
-                      <ModalPeriod setFunctionParent={(dateI, dateF) => {
-                        const updatedProcesses = processes.map((p, idx) => idx === index ? { ...p, start: dateI, end: dateF } : p);
-                        setProcesses(updatedProcesses);
-                      }} index={index} />
+                      <ModalPeriod
+                        setFunctionParent={(dateI, dateF) => {
+                          const updatedProcesses = processes.map((p, idx) =>
+                            idx === index
+                              ? { ...p, start: dateI, end: dateF }
+                              : p,
+                          );
+                          setProcesses(updatedProcesses);
+                        }}
+                        index={index}
+                      />
                     )}
                   </div>
                 </div>
                 <div className="flex w-full justify-end">
-                  <ModalDeleteUser store_user_id={process.id} user_name={process.name} />
+                  <ModalDeleteUser
+                    store_user_id={process.id}
+                    user_name={process.name}
+                  />
                 </div>
               </div>
             </div>
@@ -151,8 +191,13 @@ const ProcessTab = () => {
 
       <div className="mt-10 flex w-full flex-1 items-end px-6">
         <div className="flex w-full justify-between">
-          <label className="text-xs font-light text-[#8F8F8F]">Actualizado 07 septiembre 2024</label>
+          <label className="text-xs font-light text-[#8F8F8F]">
+            Actualizado 07 septiembre 2024
+          </label>
         </div>
+        <button className="h-[31px] px-4  rounded-xl bg-[#E0E0E0] text-xs font-semibold text-[#44444F] hover:bg-[#E0E0E0]">
+          Guardar
+        </button>
       </div>
     </div>
   );
--- src/pages/Inventory/GeneralServices/Components/Forms/ShoppingForm.jsx
+++ src/pages/Inventory/GeneralServices/Components/Forms/ShoppingForm.jsx
@@ -19,28 +19,30 @@ import {
 } from "@/components/ui/popover";
 import SelectRouter from "@/layouts/Masters/FormComponents/select";
 
-const  ShoppingTab = () => {
- 
-
+const ShoppingTab = () => {
   return (
     <div className="w-full rounded-xl bg-white p-4 md:max-h-[320px]">
       <div className="flex w-full flex-wrap gap-4">
         <div className="flex w-full flex-col gap-6">
-          <div className="flex flex-col w-full gap-6">
-          <h2 className="font-poppins ml-2 text-sm font-medium text-[#44444F]">COMPRAS</h2>
+          <div className="flex w-full flex-col gap-6">
+            <h2 className="ml-2 font-poppins text-sm font-medium text-[#44444F]">
+              COMPRAS
+            </h2>
             {/* Código de Artículo */}
-            <SelectRouter
-              placeholder="Proveedor Predeterminado:"
-      
-            /><div className="mt-[500px] flex w-full flex-1 items-end px-6">
-            <div className="flex w-full justify-between">
-              <label className="text-xs font-light text-[#8F8F8F]">Actualizado 07 septiembre 2024</label>
+            <SelectRouter placeholder="Proveedor Predeterminado:" />
+            <div className="mt-[500px] flex w-full flex-1 items-end px-6">
+              <div className="flex w-full justify-between">
+                <label className="text-xs font-light text-[#8F8F8F]">
+                  Actualizado 07 septiembre 2024
+                </label>
+                <button className="h-[31px] rounded-xl bg-[#E0E0E0] text-xs font-semibold text-[#44444F] px-4 hover:bg-[#E0E0E0]">
+                  Guardar
+                </button>
+              </div>
             </div>
-          </div>
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
