35eeea6a295831ee692f7b17a63305e5a310fc41
Agustin Hernandez Montero <agdehernandezmo@ittepic.edu.mx>
2024-10-14 14:48:37 -0700

    agustin pos mainPos grid avance

--- src/pages/Pos/MainPos/Components/ProductPosGrid.jsx
+++ src/pages/Pos/MainPos/Components/ProductPosGrid.jsx
@@ -19,10 +19,10 @@ function ProductsPosGrid({
   clientsOptions,
 }) {
   return (
-    <div className="grid grid-cols-12">
+    <div className="flex h-full w-full">
       {/* add */}
-      <div className="col-span-9 grid w-full grid-cols-12 gap-10">
-        <div className="col-span-12 flex flex-col gap-y-4">
+      <div className="flex w-full flex-col gap-y-4 p-4">
+        <div className="flex flex-col gap-y-4">
           <SelectRouter
             className="w-full rounded-3xl border-0 bg-[#FBFBFB] font-roboto text-xs font-light text-grisText shadow-[0px_0px_8px_1px_rgba(0,0,0,0.2)] !ring-0 !ring-offset-0 focus:border-primarioBotones"
             name={"article"}
@@ -46,17 +46,37 @@ function ProductsPosGrid({
             Categorías
           </h2>
         </div>
+        {/* categorias */}
+
+        <div className="flex flex-wrap gap-4 overflow-auto">
+          {Array(50)
+            .fill()
+            .map((e, index) => (
+              <div
+                key={index}
+                className="cursor-pointer text-roboto flex h-[100px] w-[169px] items-center justify-center rounded-xl bg-primarioBotones text-xl font-semibold text-white"
+              >
+                Calzado
+              </div>
+            ))}
+        </div>
       </div>
-      <div className="col-span-3">
-        <div className="grid grid-cols-12">
-          <div className="col-span-3">Cliente</div>
-          <div className="col-span-6">José Saturdino Cardozo</div>
-          <div className="col-span-3">
+      <div className="w-full max-w-[413px] border-l border-[#D7D7D7] p-4">
+        <div className="flex w-full max-w-[413px] flex-row justify-between">
+          <div className="flex gap-x-4">
+            <div className="w-fit font-poppins text-lg font-normal text-grisHeading">
+              Cliente
+            </div>
+            <div className="w-fit font-poppins text-base font-medium text-grisText">
+              José Saturdino Cardozo
+            </div>
+          </div>
+          <div className="w-fit">
             <Button
               type="button"
-              className="flex h-fit w-fit cursor-pointer items-center justify-center rounded-xl bg-primarioBotones px-3 py-1"
+              className="flex h-[24px] w-fit cursor-pointer items-center justify-center gap-x-2 rounded-xl bg-primarioBotones px-2 py-0 text-[11px] font-medium"
             >
-              <IonIcon icon={add} className="h-5 w-5"></IonIcon>
+              <IonIcon icon={add} className="h-4 w-4"></IonIcon>
               Cliente
             </Button>
           </div>
