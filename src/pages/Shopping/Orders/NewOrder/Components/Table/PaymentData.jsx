f382a02d407574c07c093723b66f062ca5bbaa75
Bartolo <123692950+AntonioEAB@users.noreply.github.com>
2024-10-14 13:57:01 -0600

    updated request
    changes to style and redirect

--- src/pages/Shopping/Orders/NewOrder/Components/Table/PaymentData.jsx
+++ src/pages/Shopping/Orders/NewOrder/Components/Table/PaymentData.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import {
   Table,
@@ -10,14 +10,27 @@ import {
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 
-const PaymentDataTable = () => {
+const PaymentDataTable = ({ paymentData, onReady }) => {
   return (
     <div className="flex h-full flex-col rounded-xl bg-white">
       {/* Header */}
       <div className="flex items-center justify-between gap-x-10 border-b border-[#E8E8E8] px-6 py-3">
         <span className="font-poppins text-lg font-medium text-[#44444F]">
-          INFORMES DE PAGOS
+          INFORMES DE RECIBO
         </span>
+      </div>
+
+      {/* Recibo Card */}
+      <div className="p-6 ml-4 mt-4 w-[280px] h-[50px] flex items-center justify-between rounded-[14px] bg-[#F1F1F1]">
+        <div className="flex flex-col justify-start">
+          <span className="text-left font-roboto text-sm font-semibold text-[#44444F]">
+            Recibo 1 - P1978
+          </span>
+          <span className="text-left text-xs font-light text-[#8F8F8F]">
+            Fecha de entrega: 12 09 24
+          </span>
+        </div>
+        <span className="text-xs font-light text-[#8F8F8F]">14:36</span>
       </div>
 
       {/* Scrollable Body */}
@@ -54,36 +67,25 @@ const PaymentDataTable = () => {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell>Naranja</TableCell>
-                <TableCell>28</TableCell>
-                <TableCell>26</TableCell>
-                <TableCell>CAJA</TableCell>
-                <TableCell>$400,000</TableCell>
-                <TableCell className={"font-roboto text-sm text-[#44444F]"}>
-                  <a className="border-b border-[#44444F] w-25">BO-1978</a>
-                </TableCell>
-                <TableCell></TableCell>
-                <TableCell>04-06-2024</TableCell>
-                <TableCell className={"font-roboto text-sm text-[#5B89FF]"}>
-                  Ver Documento
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Lima</TableCell>
-                <TableCell>28</TableCell>
-                <TableCell>26</TableCell>
-                <TableCell>CAJA</TableCell>
-                <TableCell>$400,000</TableCell>
-                <TableCell className={"font-roboto text-sm text-[#44444F]"}>
-                  <a className="border-b border-[#44444F] w-25">BO-1978</a>
-                </TableCell>
-                <TableCell></TableCell>
-                <TableCell>04-06-2024</TableCell>
-                <TableCell className={"font-roboto text-sm text-[#5B89FF]"}>
-                  Ver Documento
-                </TableCell>
-              </TableRow>
+              {paymentData.map((payment, index) => (
+                <TableRow key={index}>
+                  <TableCell>{payment.item}</TableCell>
+                  <TableCell>{payment.cantidadPedida}</TableCell>
+                  <TableCell>{payment.cantidadRecibida}</TableCell>
+                  <TableCell>{payment.unidad}</TableCell>
+                  <TableCell>{payment.montoRecibido}</TableCell>
+                  <TableCell className={"font-roboto text-sm text-[#44444F]"}>
+                    <a className="w-25 border-b border-[#44444F]">
+                      {payment.backOrder}
+                    </a>
+                  </TableCell>
+                  <TableCell>{payment.quienRecibio}</TableCell>
+                  <TableCell>{payment.fechaRecibo}</TableCell>
+                  <TableCell className={"font-roboto text-sm text-[#5B89FF]"}>
+                    <Link to={``}>Ver Documento</Link>
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </div>
@@ -97,6 +99,7 @@ const PaymentDataTable = () => {
         <Button
           className="h-[31px] rounded-xl bg-[#E0E0E0] px-6 py-4 text-xs font-semibold text-[#44444F] hover:bg-[#E0E0E0]"
           variant="outline"
+          onClick={onReady}
         >
           Listo
         </Button>
