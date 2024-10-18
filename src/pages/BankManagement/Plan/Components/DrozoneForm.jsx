bacd9428827481db423bc31283c84d697b599d63
Bartolo <123692950+AntonioEAB@users.noreply.github.com>
2024-10-18 16:08:51 -0600

    Update Chages styles

--- src/pages/BankManagement/Plan/Components/DrozoneForm.jsx
+++ src/pages/BankManagement/Plan/Components/DrozoneForm.jsx
@@ -40,7 +40,7 @@ const InputWithDropzone = ({
           type="text"
           value={input.value}
           onChange={(e) => onInputChange(input.id, e.target.value)}
-          className="mb-2 mt-[6px] flex h-[54px] w-full flex-grow rounded-[12px] border-none bg-transparent pl-2 placeholder:font-poppins placeholder:font-normal placeholder:text-[#D7D7D7] placeholder:text-[#CCCCCC] placeholder:text-[11px] focus:outline-none focus:ring-0"
+          className={`mb-2 mt-[6px] flex  w-full flex-grow rounded-[12px] border-none bg-transparent pl-2 placeholder:font-poppins placeholder:font-normal placeholder:text-[#D7D7D7] placeholder:text-[#CCCCCC] placeholder:text-[11px] focus:outline-none focus:ring-0${!input.value ? "h-[50px]" : "h-[54px]"}`}
           placeholder="Agregar titulo"
         />
       </div>
