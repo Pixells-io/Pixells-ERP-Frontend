52695d86e8233f54eba0389ed5bddca25cd1bac7
Agustin Hernandez Montero <agdehernandezmo@ittepic.edu.mx>
2024-10-21 17:44:05 -0700

    agustin add scroll dashboard crm

--- src/pages/CRM/Leads/Dashboard/MainDashboardCrm.jsx
+++ src/pages/CRM/Leads/Dashboard/MainDashboardCrm.jsx
@@ -300,10 +300,10 @@ function MainDashboardCrm() {
         </div>
       </div>
       {/*  */}
-      <div className="flex h-screen w-full gap-4 overflow-auto">
+      <div className="flex w-full gap-4 overflow-auto">
         {infoStages.map((stage, i) => (
           <div
-            className={`h-full w-60 flex-shrink-0 rounded-xl px-2 py-4`}
+            className={`h-full w-60 flex-shrink-0 rounded-xl px-2 py-4 overflow-auto`}
             key={i}
             style={
               stage.color != null
